3b3bf6d5a983c5cfc2e698a92c97e2b2c0376412
zhd-rudals <alexis0719@naver.com>
2024-09-09 14:34:54 +0900

    Update market_info_North.js

--- market_info_North.js
+++ market_info_North.js
@@ -1,5 +1,14 @@
 // 충주, 단양, 제천
 
 var polygonPath_North = {
-    
+    "충주 농산물종합시장" : [ 
+new kakao.maps.LatLng(37.00495902057727, 127.9218682240024 ), 
+new kakao.maps.LatLng(37.0048663023941, 127.9217904133372 ), 
+new kakao.maps.LatLng(37.00372550418026, 127.9218579026277 ), 
+new kakao.maps.LatLng(37.00364758115712, 127.9219340110519 ), 
+new kakao.maps.LatLng(37.00370668859516, 127.9239693312392 ), 
+new kakao.maps.LatLng(37.00378428138118, 127.9240820267667 ), 
+new kakao.maps.LatLng(37.00497946216423, 127.9240434553363 ), 
+new kakao.maps.LatLng(37.00508094041957, 127.9239168007224 ), 
+], 
 }
